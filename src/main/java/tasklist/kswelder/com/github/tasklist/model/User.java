2d8130da5316906f695d924609313857eb9b8040
kswelder <welderhayne2000@gmail.com>
2023-06-07 22:05:31 -0300

    User Model

---
+++ src/main/java/tasklist/kswelder/com/github/tasklist/model/User.java
@@ -0,0 +1,29 @@
+package tasklist.kswelder.com.github.tasklist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "clients")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
